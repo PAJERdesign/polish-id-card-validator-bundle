69068869a09a021bc990e791f045534bdb7a7551
Robert Pajer <robert.pajer@tolino.pl>
2022-12-02 10:06:19 +0100

    Initial commit

---
+++ src/PolishIdCardNumberValidatorBundle.php
@@ -0,0 +1,12 @@
+<?php
+namespace PAJERdesign\PolishIdCardNumberValidatorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Robert Pajer
+ */
+class PolishIdCardNumberValidatorBundle extends Bundle
+{
+
+}
